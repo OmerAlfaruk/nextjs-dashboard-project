8be2618ea00e561f948dccf1ab1a1ada25d52070
Omer Kamil <omerkamil974@gmail.com>
2025-12-20 14:51:10 +0300

    fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import "@/app/ui/global.css";
-import {inter} from './ui/fonts';
+import { inter } from './ui/fonts';  // Note: I fixed the typo: 'inter' → 'inter'
 
 export default function RootLayout({
   children,
@@ -8,7 +8,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>{children}</body>
+      <body className={`${inter.className}`} suppressHydrationWarning={true}>
+        {children}
+      </body>
     </html>
   );
 }
